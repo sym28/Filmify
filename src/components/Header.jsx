ab46c143dc0ee91f76c64f0fd2faa39e11138215
sym28 <sym28@users.noreply.github.com>
2024-08-28 17:37:07 +0000

    deploy: dce0b1006fa7b741a4946f394b1145bc2278e48f

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <header className='text-4xl font-bold flex justify-center m-10'>
-      <h1>Filmify</h1>
-    </header>
-  )
-}
